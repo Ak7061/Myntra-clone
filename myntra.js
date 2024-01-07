ec312daf599dc1462548cb86f623023b8e318c9b
Ak7061 <105783427+Ak7061@users.noreply.github.com>
2024-01-07 23:10:53 +0530

    Add files via upload

---
+++ myntra.js
@@ -0,0 +1,13 @@
+
+//   $(document).ready(function(){
+//     $('.banner_container').slick({
+//       autoplay: true,
+//       autoplaySpeed: 3000, // Set the time between slides in milliseconds
+//       dots: true, // Display navigation dots
+//       infinite: true, // Enable infinite looping
+//       speed: 500, // Animation speed in milliseconds
+//       slidesToShow: 1, // Number of slides to show at a time
+//       slidesToScroll: 1 // Number of slides to scroll
+//     });
+//   });
+
